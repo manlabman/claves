793fda51fe7ec28abe97f11cf6cd31b803dd1553
manlabman <manlabman@gmail.com>
2025-01-12 12:23:48 +0000

    cuar_comm

--- gurai_01.c
+++ gurai_01.c
@@ -1,7 +1,18 @@
 #include<iostream>
 #include<libstd.h>
 int main (){
+    int *pul = k;
+    z
+
     char *p=files;
+    for(i=0;i<8;ii++);
+    printf(pulsarr el primer boton\n;&b);
+    if(b1==100%);
+    break;
+    activar buzz
+    if(b1,)
+
+
     return 0;
 
 }
