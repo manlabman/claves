d2360b714b5a68fcb54bc95fece0372a5712e588
manlabman <manlabman@gmail.com>
2025-01-12 19:50:40 +0000

    quin_com

--- gurai_01.c
+++ gurai_01.c
@@ -1,11 +1,18 @@
 #include<iostream>
 #include<libstd.h>
 int main (){
+    in Total=100
     int *pul = k;
-    z
+    floqt Sum=incc[i];
+    Porcc=sum
 
     char *p=files;
-    for(i=0;i<8;ii++);
+    for(i=0;i<100;ii++);
+    for(j=0;j<8;j++);
+    
+    iff(i=0){
+        j[]=
+    
     printf(pulsarr el primer boton\n;&b);
     if(b1==100%);
     break;
